c492a66099d5a4a44cba4fe29ce8392b288c4530
Anton Golomazov <golomazov-tosha2012@yandex.ru>
2018-12-01 21:58:26 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<p>
+						Edit <code>src/App.js</code> and save to reload.
+					</p>
+					<div className="App-link">I was changed!</div>
+				</header>
+			</div>
+		);
+	}
+}
+
+export default App;
